7af1247cba62923a6dcdfe451c9ea55f2c41e77e
heimin22 <fyketonel@gmail.com>
2025-03-25 21:22:12 +0800

    A small calculator hehe

--- cppVariables.cpp
+++ cppVariables.cpp
@@ -2,9 +2,32 @@
 using namespace std;
 
 int main() {
-    int x = 10;
-    int y = 10; 
-    
-    cout << x + y;
+    double num1, num2;
+    char operation;
+
+    cout << "Enter ka ng equation (ito format: a + b):";
+    cin >> num1 >> operation >> num2;
+
+    if (operation == '+' ) {
+        cout << "Result: " << num1 + num2 << endl;
+    }
+    else if (operation == '-' ) {
+        cout << "Result: " << num1 - num2 << endl;
+    }
+    else if (operation == '*' || operation == 'x' || operation == 'X') {
+        cout << "Result: " << num1 * num2 << endl;
+    }
+    else if (operation == '/') {
+        if (num2 == 0) {
+            cout << "Error: Division by zero!" << endl;
+        }
+        else {
+            cout << "Result: " << num1 / num2 << endl;
+        }
+    }
+    else {
+        cout << "Gago bawal yan hahaha. +, * , -, at / lang pwede bobo" << endl;
+    }
+
     return 0;
 }
